0440759310d0644f4e186c6901c71323b8653f9d
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-04-21 22:06:51 +0530

    #include<stdio.h> int main() {     int n1=0,n2=1,n3=0,n,i,flag=0;     scanf("%d",&n);    // printf("%d %d",n1,n2);    flag=0;     for(i=2;i<n;i++)     {         n3=n1+n2;         if(n3==n)         {             flag=1;         }         n1=n2;         n2=n3;     }     if(flag==1)     {         printf("True");     }     else     {         printf("False");     } }

--- Fibonacci_or_not.c
+++ Fibonacci_or_not.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,n1=0,n2=1,n3,i,c=0;
+    int n1=0,n2=1,n3=0,n,i,flag=0;
     scanf("%d",&n);
+   // printf("%d %d",n1,n2);
+   flag=0;
     for(i=2;i<n;i++)
     {
         n3=n1+n2;
         if(n3==n)
         {
-            c=c+1;
+            flag=1;
         }
         n1=n2;
         n2=n3;
     }
-    if(c==1)
+    if(flag==1)
     {
         printf("True");
     }
