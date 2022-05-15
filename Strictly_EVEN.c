899155ff41172fc272b7363c650f7c2878ac1d8e
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-05-15 14:35:12 +0530

    #include<stdio.h> int main() {     int a[100],i,n,k=0,c=0,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0 && a[i]%2==0)         {             c=c+1;         }     }     //printf("%d",c);     for(j=0;j<n;j++)     {         if(a[j]%2==0)         {             k=k+1;         }     }     //printf("%d",k);     if(k==c)     {         printf("True");     }     else     {         printf("False");     } }

--- Strictly_EVEN.c
+++ Strictly_EVEN.c
@@ -1,31 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    int arr[100],n,i,c=0,j,k;
+    int a[100],i,n,k=0,c=0,j;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++)
     {
-    	if(i%2==0 && arr[i]%2==0)
-    	{
-    		c=c+1;
-		}
-		//else if(i%2!=0 && arr[i]%2!=0)
-		//{
-		 //   k=k+1;
-		//}
-	}
-	for(i=0;i<n;i++)
-	{
-	    if(arr[i]%2==0)
-	    {
-	        k=k+1;
-	    }
-	}
-    if(c==k)
+        if(i%2==0 && a[i]%2==0)
+        {
+            c=c+1;
+        }
+    }
+    //printf("%d",c);
+    for(j=0;j<n;j++)
+    {
+        if(a[j]%2==0)
+        {
+            k=k+1;
+        }
+    }
+    //printf("%d",k);
+    if(k==c)
     {
         printf("True");
     }
