b93f6fe7aed6bc7c91752640dd69e0e0a004ad9d
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-05-16 23:35:51 +0530

    #include<stdio.h> int main() { 	int a[10][10],t[10][10],r,c,i,j,l; 	scanf("%d%d",&r,&c); 	for(i=0;i<r;i++) 	{ 		for(j=0;j<c;j++) 		{ 			scanf("%d",&a[i][j]); 		} 	} 	for(i=0;i<r;i++) 	{ 		for(j=0;j<c;j++) 		{ 			t[j][i]=a[i][j]; 		} 	} 	for(i=0;i<c;i++) 	{ 		l=0; 		for(j=0;j<r;j++) 		{ 			if(l<t[i][j]) 			{ 				l=t[i][j]; 		    } 		} 		printf("%d",l); 		printf(" "); 	} }

---
+++ Maximum_element_in_each_column.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],t[10][10],r,c,i,j,l;
+	scanf("%d%d",&r,&c);
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			t[j][i]=a[i][j];
+		}
+	}
+	for(i=0;i<c;i++)
+	{
+		l=0;
+		for(j=0;j<r;j++)
+		{
+			if(l<t[i][j])
+			{
+				l=t[i][j];
+		    }
+		}
+		printf("%d",l);
+		printf("
+");
+	}
+}
