b3ec5a5d29b0e5ba9d59a9ac24af2801b063b84d
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-05-19 22:29:20 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         printf("prime");     }     else     {         printf("not a prime");     } }

--- prime_number.c
+++ prime_number.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,c=0,i;
+    int n,i,c=0;
     scanf("%d",&n);
-    for(i=1;i<n;i++)
+    for(i=1;i<=n;i++)
     {
         if(n%i==0)
         {
-            c=c+1;
+            c++;
         }
     }
-    if(c==1)
+    if(c==2)
     {
         printf("prime");
     }
